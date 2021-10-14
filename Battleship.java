2f3ce8dda6515a418f31584b57109ad024479245
houjer23 <60145875+houjer23@users.noreply.github.com>
2021-10-14 09:02:19 -0400

    Update Battleship.java

--- Battleship.java
+++ Battleship.java
@@ -12,5 +12,16 @@ public class Battleship {
 		
 	}
 	
+	// This Method is used for testing place Ship
+	public printBoard() {
+		System.out.println();
+		for (int i = 0; i < board.length; i ++) {
+			for (int j = 0; j < board[0].length; j ++) {
+				System.out.print(board[i][j] + " ");
+			}
+			System.out.println();
+		}
+		System.out.println();
+	}
 	
 }
