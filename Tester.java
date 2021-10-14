201dd3caf3d327eef9d80b3cb20333af047148ff
houjer23 <60145875+houjer23@users.noreply.github.com>
2021-10-14 09:11:00 -0400

    Update Tester.java

--- Tester.java
+++ Tester.java
@@ -1,7 +1,14 @@
 import java.util.Scanner;
+
 public class Tester {
-	public static void main(string args[]) {
-		
-		
+	public static void main(String args[]) {
+		Scanner scan = new Scanner(System.in);
+		System.out.println("\nWelcome to Battleship game!");
+		System.out.println("The computer will generate random positions for battleships");
+		System.out.println("Generating...");
+		System.out.print("Hit return to continue ");
+		scan.nextLine();
+		System.out.println("\nThe computer placed 3 ships");
+		System.out.println("Try to guess where the computer place the ship");
 	}
 }
