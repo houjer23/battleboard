5cf0852174ca6fe96790dfc0b21b160c5737de22
houjer23 <60145875+houjer23@users.noreply.github.com>
2021-10-14 08:54:38 -0400

    Update and rename Main.java to Battleship.java

---
+++ Battleship.java
@@ -0,0 +1,16 @@
+public class Battleship {
+	
+	private Set<Integer> ships;
+	int[][] board;
+	
+	public Battleboard() {
+		ships = new HashSet<>();
+		board = new int[10][10];
+	}
+	
+	public void placeShip() {
+		
+	}
+	
+	
+}
