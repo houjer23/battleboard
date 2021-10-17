a427dcaeb54ac9e2910dae227652a1d7f9d7e5c8
houjer23 <60145875+houjer23@users.noreply.github.com>
2021-10-17 13:40:55 -0400

    Update Tester.java

--- Tester.java
+++ Tester.java
@@ -1,13 +1,18 @@
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
+// import all classes in Java
 
+// Tester class -- running the game, access to Battleship class
 public class Tester {
-	public static void main(String args[]) {
-		Scanner scan = new Scanner(System.in);
+	public static void main(String args[]) { // program starts running here
+		Scanner scan = new Scanner(System.in); 
 		System.out.println("\nWelcome to Battleship game!");
 		
-		Battleship battleship = new Battleship();
-		battleship.printBoard();
+		Battleship battleship = new Battleship(); // construct Battleship
+		battleship.placeShip(3); // calling placeShip method in battleship class
+		battleship.printBoard(); // calling printBoard method in battleship class
 		
+		// Just some prompts here
 		System.out.println("The computer will generate random positions for battleships");
 		
 		
@@ -16,5 +21,5 @@ public class Tester {
 		scan.nextLine();
 		System.out.println("\nThe computer placed 3 ships");
 		System.out.println("Try to guess where the computer place the ship");
-	}
-}
+	} // end of the program
+} // end of Tester class
