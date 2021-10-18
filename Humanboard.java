5eb43d27ed0b89d55168fedbfd165776af6c5ec3
Kevinchen938953 <59925517+Kevinchen938953@users.noreply.github.com>
2021-10-18 19:19:08 -0400

    Update Humanboard.java

--- Humanboard.java
+++ Humanboard.java
@@ -8,6 +8,9 @@ public class Humanboard
 		humanboard = new int[10][10]; // initialize the board to size 10 * 10
 		computerguess = new int[10][10]; // initialize the guess to size 10 * 10
 		posGuessed = 0;
+	        int determine;
+	        int humancol;
+	        int humanrow;
        } // end of construction method
 		
 		  
@@ -16,11 +19,11 @@ public class Humanboard
 	{ // method place ship helps to place the ship to the board n times
 		Scanner getdetermine = new Scanner(System.in);
 		System.out.print("Select numbers 0 or 1");
-		int determine = getdetermine.nextInt();
+		determine = getdetermine.nextInt();
 		   for (int i = 0; i < n; i ++) 
 		     { // place ship n times
 			 // random number (0 or 1)
-			if (getdetermine == 0) 
+			if (determine == 0) 
 			{ // if the random number is 0, place the ship horizontally (call the helper method)
 				if(placeShipHorizontalhuman()==false)
 				{
@@ -41,9 +44,9 @@ public class Humanboard
 	{  
 		Scanner getcol = new Scanner(System.in);
 		System.out.print("Select col");
-		int humancol = getcol.nextInt();
+		humancol = getcol.nextInt();
 		System.out.print("select row");
-		int humanrow = getcol.nextInt();
+		humanrow = getcol.nextInt();
 		// helper method for place ship (place ship horizontally)
 		// the following shipRow and ship Column are the starting position of a ship 
 		// choose a random number for row from 0 to board length (in this case is 9) -- this doesn't change for ship
