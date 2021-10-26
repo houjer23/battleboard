5bc224eb6b1961794da9b56aca5433ca6102cfec
houjer23 <60145875+houjer23@users.noreply.github.com>
2021-10-26 08:32:44 -0400

    Update Humanboard.java
    Fix bugs

--- Humanboard.java
+++ Humanboard.java
@@ -3,27 +3,22 @@ import java.util.*;
 
 public class Humanboard
 {
-     public Humanboard() 
-      { // construction method
+     public Humanboard() { // construction method
 		humanboard = new int[10][10]; // initialize the board to size 10 * 10
 		computerguess = new int[10][10]; // initialize the guess to size 10 * 10
 		posGuessed = 0;
-	        int determine;
-	        int humancol;
-	        int humanrow;
-       } // end of construction method
+     } // end of construction method
 		
 		  
 		 
-      public void placeShiphuman(int n) 
-	{ // method place ship helps to place the ship to the board n times
+      public void placeShiphuman(int n) { // method place ship helps to place the ship to the board n times
 		Scanner getdetermine = new Scanner(System.in);
 		System.out.print("Select numbers 0 or 1");
-		determine = getdetermine.nextInt();
+		int determine = getdetermine.nextInt();
 		   for (int i = 0; i < n; i ++) 
 		     { // place ship n times
 			 // random number (0 or 1)
-			if (determine == 0) 
+			if (getdetermine == 0) 
 			{ // if the random number is 0, place the ship horizontally (call the helper method)
 				if(placeShipHorizontalhuman()==false)
 				{
@@ -44,9 +39,9 @@ public class Humanboard
 	{  
 		Scanner getcol = new Scanner(System.in);
 		System.out.print("Select col");
-		humancol = getcol.nextInt();
+		int humancol = getcol.nextInt();
 		System.out.print("select row");
-		humanrow = getcol.nextInt();
+		int humanrow = getcol.nextInt();
 		// helper method for place ship (place ship horizontally)
 		// the following shipRow and ship Column are the starting position of a ship 
 		// choose a random number for row from 0 to board length (in this case is 9) -- this doesn't change for ship
@@ -77,7 +72,7 @@ public class Humanboard
 		}
 		for (int i = shipRow; i <= shipRow+2; i++) 
 		 {
-			humanboard[i][humancol]==1
+			humanboard[i][humancol]=1;
 			
 			
 			
@@ -85,7 +80,3 @@ public class Humanboard
 		return true; 
 	}
   }
- }
-
-}
-
