f9e5a38e7aca0c135647a9e9641ef538a34344f9
houjer23 <60145875+houjer23@users.noreply.github.com>
2021-10-28 08:44:32 -0400

    Update Tester.java
    Distinguish between human board and computer board

--- Tester.java
+++ Tester.java
@@ -10,20 +10,20 @@ public class Tester {
 		System.out.println("The computer will generate random positions. If you guess correctly, you got a HIT");
 		System.out.println("A ship is sunk when all its positions are hit");
 		
-		Battleship battleship = new Battleship(); // construct Battleship
+		Computer_Ship computer_board = new Computer_Ship(); // construct Battleship
 		
 		System.out.println("The computer will generate random positions for battleships");
 		System.out.println("Generating...");
-		battleship.placeShip(); // calling placeShip method in battleship class
+		computer_board.placeShip(); // calling placeShip method in battleship class
 		
-		/*
-		for (int i = 0; i < battleship.board.length; i ++) {
-			for (int j = 0; j < battleship.board.length; j ++) {
-				System.out.print(battleship.board[i][j] + " ");
+		
+		for (int i = 0; i < computer_board.board.length; i ++) {
+			for (int j = 0; j < computer_board.board.length; j ++) {
+				System.out.print(computer_board.board[i][j] + " ");
 			}
 			System.out.println();
 		}
-		*/
+		
 		
 		System.out.print("Hit return to continue ");
 		scan.nextLine();
@@ -33,11 +33,11 @@ public class Tester {
 		System.out.println("Guess the ship");
 		System.out.println("If you hit the ship, it will be indicated by *");
 		System.out.println("If you doesn't hit the ship, it will be indicated by X");
-		while (battleship.posGuessed < 9) {
-			battleship.printBoard(); // calling printBoard method in battleship class
-			battleship.checkBoard(); // calling checkBoard method in battleship class
+		while (computer_board.posGuessed < 9) {
+			computer_board.printBoard(); // calling printBoard method in battleship class
+			computer_board.checkBoard(); // calling checkBoard method in battleship class
 		}
-		battleship.printBoard();
+		computer_board.printBoard();
 		System.out.println("You win the game");
 	} // end of the program
 } // end of Tester class
