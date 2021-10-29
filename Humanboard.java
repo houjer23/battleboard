c99fc276fc8218c5e1e287cfd1ef392ea1b01705
houjer23 <60145875+houjer23@users.noreply.github.com>
2021-10-29 15:31:43 -0400

    Update Humanboard.java
    Adding more comments

--- Humanboard.java
+++ Humanboard.java
@@ -1,36 +1,37 @@
+/*
+ *  By Kevin and Jerry
+ *  Humanboard class -- human place the ship and computer guess the ship
+ */
+
 import java.io.*;
 import java.util.*;
 // import all classes in Java
 
-// Battleship class -- all methods and actions of battleships are written here
 public class Humanboard {
 	
-	int[][] human_board; //  Board stores the locations of ships
-	int[][] computer_guess; // guess stores the user input locations
+	int[][] human_board; //  human_board stores the locations of ships
+	int[][] computer_guess; // It stores computer guess locations
 	int posGuessed; // stores how many ships are hit
-	int[] ships = {2, 3, 4};
 	
 	public Humanboard() { // construction method
 		human_board = new int[10][10]; // initialize the board to size 10 * 10
 		computer_guess = new int[10][10]; // initialize the guess to size 10 * 10
-		posGuessed = 0;
+		posGuessed = 0; // the amount of ships already guess
 	} // end of construction method
 	
 	public void placeShip() { // method place ship helps to place the ship to the board n times
-		System.out.println("Please select column and row. Computer will guess your board");
-		for (int i = 2; i <= 4; i ++) 
-		{
+		System.out.println("Please select column and row of the starting position of your ship. Computer will guess your board");
+		for (int i = 2; i <= 4; i ++) {
 			print_Humanboard();
-			
 			System.out.println("This ship will be size " + i);
+			// select vertical or horizontal
 			int vertical_or_horizontal;
 		    do {
 			   Scanner get_num = new Scanner(System.in);
 			   System.out.print("Select 0 for horizontal ship and 1 for vertical ship: ");
 			   vertical_or_horizontal = get_num.nextInt();
 			} while (vertical_or_horizontal != 1 && vertical_or_horizontal != 0); //restricts user input to 0 and 1;
-			
-			
+			// input row and column
 			int shipRow;
 			int shipColumn;
 			Scanner scan = new Scanner(System.in);
@@ -38,59 +39,36 @@ public class Humanboard {
 			shipRow = scan.nextInt();
 			System.out.print("What row do you want? ");
 			shipColumn = scan.nextInt();
-			/*
-			do {
-			  Scanner scan = new Scanner(System.in);
-			  System.out.print("What column do you want? ");
-			  shipRow = scan.nextInt();
-			  System.out.print("What row do you want? ");
-			  shipColumn = scan.nextInt();
-			  
-			  
-			  boolean good_position;
-				if (vertical_or_horizontal == 0) {
-					if ((shipRow < 1 && shipRow > 9) || (shipColumn < 1 && shipRow > 9 - i)) {
-						
-					}
-				} else {
-					
-				}
-			
-			
-			
-			} while(((vertical_or_horizontal == 0) && ((shipRow < 1 || shipRow > 9) || (shipColumn < 1 || shipRow > 9 - i))) || ((vertical_or_horizontal == 0) && ((shipRow < 1 && shipRow > 9 - i) && (shipColumn < 1 && shipRow > 9))) ); //restricts user input to number between 1 and 9
-		
-			*/
-			if(vertical_or_horizontal==0)
+			// call placeship helper
+			if(vertical_or_horizontal == 0)
 			{
-				placeShipHorizontal(i, shipRow, shipColumn);//Calling method to place ship horizontal
+				placeShipHorizontal(i, shipRow, shipColumn);
 		    }
 		    else
 		    {
-				placeShipVertical(i, shipRow, shipColumn);//Passes method with arguments to place ship vertical
+				placeShipVertical(i, shipRow, shipColumn);
 		    }
-		} // end of for loop (place ship n times)
+		}
 	} // end of the place ship method
 	
 	
 	public void placeShipHorizontal(int size, int shipRow, int shipColumn) {
 		for (int i = shipColumn; i < shipColumn + size; i++) { // column number add 1 each time
-			human_board[shipRow][i] = 1; // If these locations are not placed already, set the whole ship, indicated by 1 in the board
+			human_board[shipRow][i] = 1; // set the whole ship, indicated by 1 in the board
 		} // end of the for loop
 	} // end of helper method for place ship (place ship horizontally)
 	
 	public void placeShipVertical(int size, int shipRow, int shipColumn) {
 		for (int i = shipRow; i < shipRow+size; i++) {  // row number add 1 each time
-			human_board[i][shipColumn] = 1;  // If these locations are not placed already, set the whole ship, indicated by 1 in the board			
+			human_board[i][shipColumn] = 1;  // set the whole ship, indicated by 1 in the board			
 		} // end of the for loop
-		
-	}
+	} // end of helper method for place ship (place ship vertically)
 	
-	public void print_Humanboard() {
+	public void print_Humanboard() { // This method prints the board to give hints of where the human puts 
 		System.out.println();
 		System.out.print(" ");
 		for (int i = 0; i < human_board.length; i ++) {
-			System.out.print(" " + i); 
+			System.out.print(" " + i);
 		}
 		System.out.println();
 		for (int i = 0; i < human_board.length; i ++) { // looping through rows
@@ -100,11 +78,11 @@ public class Humanboard {
 				System.out.print(human_board[i][j] + " ");
 			} // end of looping through columns
 			System.out.println();
-		} // end of looping through rows 
+		} // end of looping through rows
 		System.out.println();
-	}
+	} // end of print_Humanboard
 	
-	public void printBoard() { // This method prints the board with user guess
+	public void printBoard() { // This method prints the board with computer guess
 		System.out.println();
 		System.out.print(" ");
 		for (int i = 0; i < computer_guess.length; i ++) {
@@ -128,22 +106,19 @@ public class Humanboard {
 		System.out.println();
 	} // end of print board method
 	
-	public void checkBoard() { // This method checks user input if matches with board
+	public void checkBoard() { // This method checks computer input if matches with board
+		// Gets computer guesses on rows and columns
 		int rowPos = (int)(Math.random() * 10);
-		int colPos = (int)(Math.random() * 10); // Gets user guesses on rows and columns
+		int colPos = (int)(Math.random() * 10);
 		if (computer_guess[rowPos][colPos] == 2) {
 			System.out.println("The position is already hit");
-		} else if (human_board[rowPos][colPos] == 1 || human_board[rowPos][colPos] == 2 || human_board[rowPos][colPos] == 3) { //If the guess array matches the board at the user input position, then the guess is correct
+		} else if (human_board[rowPos][colPos] == 1 || human_board[rowPos][colPos] == 2 || human_board[rowPos][colPos] == 3) { // If the guess array matches the board at the computer input position, then the guess is correct
 			System.out.println("HIT");
 			computer_guess[rowPos][colPos] = 2;
 			posGuessed ++;
-			ships[human_board[rowPos][colPos] - 1] --;
-			if (ships[human_board[rowPos][colPos] - 1] == 0) {
-				System.out.println("Ship Sank");
-			}
 		} else {
 			computer_guess[rowPos][colPos] = 1;
 			System.out.println("MISS"); // if not then it's a miss
 		}
-	}
-}
+	} // end of checkBoard methos
+} // end of the class
